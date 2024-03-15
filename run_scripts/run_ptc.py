34e57ecd243be90d633f2c4caef6c2151e87f30b
Julie Leproux <julieleproux01@gmail.com>
2024-03-15 17:32:55 +0100

    ajout code ptc

--- run_scripts/run_ptc.py
+++ run_scripts/run_ptc.py
@@ -6,16 +6,50 @@ Created on Fri Mar  8 12:35:25 2024
 @author: philippe.gris@clermont.in2p3.fr
 """
 
+import os
 from optparse import OptionParser
+from ptc import merge_data, process
+import pandas as pd
+import sys 
+sys.path.append("/home/julie/fm_study/im_pipe/python")
+
 
 parser = OptionParser(description='Script to estimate the PTC on FP images')
 
-parser.add_option('--imList', type=str,
-                  default='list_im.csv',
-                  help='List of images to process[%default]')
+parser.add_option('--flat_empty', type=str,
+                  default='/home/julie/fm_study/im_pipe/input/flat_13144_flat_empty.csv',
+                  help='[%default]')
 
+parser.add_option('--flat_ND', type=str,
+                  default='/home/julie/fm_study/im_pipe/input/flat_13144_flat_ND.csv',
+                  help='[%default]')
+
+parser.add_option('--data_path', type=str,
+                  default="/home/julie/Téléchargements/CCD2-20240307T134109Z-002/CCD2/",
+                  help='[%default]')
 opts, args = parser.parse_args()
 
-imList = opts.imList
+flat_empty = opts.flat_empty
+flat_ND = opts.flat_ND
+data_path = opts.data_path
 
-print(imList)
+
+
+
+filelist = os.listdir(data_path)
+flat = [flat_empty,flat_ND]
+
+data = merge_data(filelist,flat)
+
+resa = pd.DataFrame()
+
+for i,row in data.iterrows():
+    flat0 = data_path+row['full_path_flat0']
+    flat1 = data_path+row['full_path_flat1']
+    res = process(flat0,flat1)
+    resa = pd.concat([res,resa]).reset_index(drop=True)
+
+
+
+
+
