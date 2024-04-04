43460023d8b090a95dd06abb460d605c32c3f08e
Julie Leproux <julieleproux01@gmail.com>
2024-04-04 16:48:34 +0200

    ajout ptc

---
+++ im_tools/num.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 09:38:13 2024
+
+@author: julie
+"""
+import os
+import pandas as pd
+from ptc import fit, plot_ampli
+
+
+def GetRunNumber (path):
+    file = os.listdir(path)
+    run = pd.DataFrame(file, columns=['FileName'])
+    run['run'] = run['FileName'].str.split('_').str.get(1)
+    run['filter'] = run['FileName'].str.split('_').str.get(3).str.split('.').str.get(0)
+    return run
+
+
+def DoFit (data, path, run):
+    print(len(data))
+    if len(data)==1:
+        file = pd.read_hdf(path+data[0])
+    elif len(data)==2:
+        file = pd.concat((pd.read_hdf(path+data[0]),pd.read_hdf(path+data[1])))
+    else : 
+        return
+    
+    print(file)
+    resu_fit = fit(file, run)
+    return resu_fit
+
